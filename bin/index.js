0b0a7af70c9678a9ae0c2b1a0cee553915fe0822
yamadapc <tacla.yamada@gmail.com>
2014-11-18 13:21:36 -0500

    Add very simple set-up helper script
    I think this is a nice thing to have. This also bumps version to 0.2.0
    and republishes to npm.

---
+++ bin/index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+'use strict';
+var child = require('child_process');
+var fs = require('fs');
+var path = require('path');
+
+var cwd = process.cwd();
+var pkgjsonpth = path.join(cwd, 'package.json');
+var files = fs.readdirSync(cwd);
+if(files.indexOf('package.json') === -1) {
+  fail('No `package.json` found.');
+}
+
+var json;
+try {
+  json = require(pkgjsonpth);
+} catch(err) {
+  fail('Failed to read your `package.json`');
+}
+
+if(!json.scripts) json.scripts = {};
+if(json.scripts.test) json.scripts.test += ' &&';
+json.scripts.test = json.scripts.test || '' + 'mocha --require blanket';
+
+if(!json.config) json.config = {};
+json.config.blanket = extend({
+  'dave-cover-never': ['node_modules', 'test'],
+  'pattern': [
+    'lib', 'YOUR', 'MODULE', 'DIRECTORIES', 'HERE'
+  ],
+  'spec-cov': {
+    'threshold': 80,
+    'localThreshold': 80,
+  }
+}, json.config.blanket || {});
+
+process.stdout.write('Adding mocha-spec-cov-alt to your package.json...');
+fs.writeFileSync(pkgjsonpth, JSON.stringify(json, null, 2));
+process.stdout.write(' OK\n');
+console.log('Adding `mocha`, `mocha-spec-cov-alt` and `blanket` through npm:');
+var npm = child.spawn('npm', [
+  'install', '--save-dev', 'mocha-spec-cov-alt', 'mocha', 'blanket',
+]);
+npm.stdout.pipe(process.stdout);
+
+/**
+ * Merges two objects together, overwriting the second's properties onto the
+ * first.
+ *
+ * @param {Object} target
+ * @param {Object} source
+ * @return {Object} result The modified `target` parameter
+ */
+
+function extend(target, source) {
+  var keys = Object.keys(source);
+  for(var i = 0, len = keys.length; i < len; i++) {
+    var key = keys[i];
+    target[key] = source[key];
+  }
+  return target;
+}
+
+/**
+ * Prints error message and exits with non-zero code
+ * @param {{String|Error}
+ */
+
+function fail(err) {
+  console.error(err.message || err);
+  process.exit(err.code || 1);
+}
